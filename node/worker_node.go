1dec191252c8355e4e8e8e4aed0d7d2d7fa96647
hanxiang1 <hanxiang1@staff.sina.com.cn>
2022-12-21 11:55:12 +0800

    feat(task_queue):add task queue

--- node/worker_node.go
+++ node/worker_node.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	redis_driver "worker_tools/driver"
+	"worker_tools/utils"
 
 	"github.com/gomodule/redigo/redis"
 
@@ -18,6 +19,19 @@ import (
 type WorkerNode struct {
 	conn *grpc.ClientConn  // grpc client connection
 	c    NodeServiceClient // grpc client
+}
+
+type WorkerControl struct {
+	jobQueue *utils.JobQueue
+	wm       *utils.WorkerManager
+}
+
+// new wokerControl
+var workerControl = new(WorkerControl)
+
+func (w *WorkerControl) CommitJob(job *utils.Job) {
+	w.jobQueue.PushJob(job)
+	fmt.Println("commit job success")
 }
 
 func (n *WorkerNode) Init() (err error) {
@@ -41,6 +55,8 @@ func (n *WorkerNode) Init() (err error) {
 
 	//register worker
 	registerWorker()
+	//register TaskQueue
+	registerTaskQueue()
 	return nil
 }
 
@@ -58,36 +74,64 @@ func (n *WorkerNode) Start() {
 		if err != nil || res.Data == "" {
 			return
 		}
-
 		strCommand := res.Data
 		fmt.Println("strCommand: ", strCommand)
 
-		// execute command
-		cmd := exec.Command("/bin/bash", "-c", strCommand)
-		stdout, _ := cmd.StdoutPipe()
-		if err := cmd.Start(); err != nil {
-			fmt.Println("Execute failed when Start:" + err.Error())
-			return
+		//添加job 执行的任务
+		job := &utils.Job{
+			DoneChan: make(chan struct{}, 1),
+			HandleFunc: func(job *utils.Job) error {
+				// execute command
+				cmd := exec.Command("/bin/bash", "-c", strCommand)
+				stdout, _ := cmd.StdoutPipe()
+				if err := cmd.Start(); err != nil {
+					fmt.Println("Execute failed when Start:" + err.Error())
+					return err
+				}
+				out_bytes, _ := ioutil.ReadAll(stdout)
+				stdout.Close()
+				if err := cmd.Wait(); err != nil {
+					fmt.Println("Execute failed when Wait:" + err.Error())
+					return err
+				}
+				fmt.Println(strings.TrimSpace(string(out_bytes)))
+				return nil
+			},
 		}
-		out_bytes, _ := ioutil.ReadAll(stdout)
-		stdout.Close()
-		if err := cmd.Wait(); err != nil {
-			fmt.Println("Execute failed when Wait:" + err.Error())
-			return
-		}
-		fmt.Println(strings.TrimSpace(string(out_bytes)))
+
+		workerControl.CommitJob(job)
+		fmt.Println("commit job to job queue success")
+		job.WaitDone()
 	}
 }
 
 func registerWorker() (err error) {
 	rd := redis_driver.GetRedisInstance()
-	//设置超时时间
-	rd.SetTimeout(20 * time.Second)
+	//设置超时时间 1分钟
+	rd.SetTimeout(1 * time.Minute)
 	//注册worker
 	worker_node_key, err := rd.RegisterWorkereNode("worker_node")
 	fmt.Println("worker_node_key", worker_node_key)
 	//注册心跳
 	rd.SetHeartBeat(worker_node_key)
+	return
+}
+
+func registerTaskQueue() (err error) {
+	jobQueue := utils.NewJobQueue(10)
+	fmt.Println("init job queue success")
+
+	m := utils.NewWorkerManager(jobQueue)
+
+	//注册两个worker
+	m.CreateWorker(1)
+	m.CreateWorker(2)
+
+	workerControl = &WorkerControl{
+		jobQueue: jobQueue,
+		wm:       m,
+	}
+
 	return
 }
 
---
+++ utils/queue.go
@@ -0,0 +1,121 @@
+package utils
+
+import (
+	"container/list"
+	"fmt"
+	"sync"
+)
+
+//Job的类
+type Job struct {
+	DoneChan   chan struct{}
+	HandleFunc func(j *Job) error
+}
+
+func (job *Job) Done() {
+	job.DoneChan <- struct{}{}
+	close(job.DoneChan)
+}
+
+func (job *Job) WaitDone() {
+	select {
+	case <-job.DoneChan:
+		return
+	}
+}
+
+func (job *Job) Execute() error {
+	fmt.Println("job start to execute ")
+	return job.HandleFunc(job)
+}
+
+//任务队列
+type JobQueue struct {
+	mu sync.Mutex
+	//任务关闭使用
+	noticeChan chan struct{}
+	//任务队列
+	queue *list.List
+	//任务队列实时大小
+	size int
+	//任务队列初始化的大小
+	capacity int
+}
+
+func NewJobQueue(cap int) *JobQueue {
+	return &JobQueue{
+		capacity:   cap,
+		queue:      list.New(),
+		noticeChan: make(chan struct{}, 1),
+	}
+}
+
+func (q *JobQueue) PushJob(job *Job) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	q.size++
+	if q.size > q.capacity {
+		q.RemoveLeastJob()
+	}
+
+	q.queue.PushBack(job)
+	//通知有任务
+	q.noticeChan <- struct{}{}
+}
+
+func (q *JobQueue) PopJob() *Job {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	if q.size == 0 {
+		return nil
+	}
+
+	q.size--
+	return q.queue.Remove(q.queue.Front()).(*Job)
+}
+
+func (q *JobQueue) RemoveLeastJob() {
+	if q.queue.Len() != 0 {
+		back := q.queue.Back()
+		abandonJob := back.Value.(*Job)
+		abandonJob.Done()
+		q.queue.Remove(back)
+	}
+}
+
+func (q *JobQueue) waitJob() <-chan struct{} {
+	return q.noticeChan
+}
+
+type WorkerManager struct {
+	jobQueue *JobQueue
+}
+
+func NewWorkerManager(jobQueue *JobQueue) *WorkerManager {
+	return &WorkerManager{
+		jobQueue: jobQueue,
+	}
+}
+
+func (m *WorkerManager) CreateWorker(workerName int) error {
+
+	go func(index int) {
+		fmt.Println("start worker success")
+		var job *Job
+
+		for {
+			select {
+			case <-m.jobQueue.waitJob():
+				fmt.Println("get a job from job queue No.", index)
+				job = m.jobQueue.PopJob()
+				fmt.Println("start to execute job No.", index)
+				job.Execute()
+				fmt.Print("job done")
+				job.Done()
+			}
+		}
+	}(workerName)
+
+	return nil
+}
